e00d102bd615e8fff048752103fea341364c5f85
natty-light <75773163+natty-light@users.noreply.github.com>
2022-12-17 19:25:35 -0500

    Updated next.config.js

--- tbrowne.me/next.config.js
+++ tbrowne.me/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
