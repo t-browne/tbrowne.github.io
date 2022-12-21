3a87a4a56c5900b42fc372d0ed043b040c6fd95c
t-browne <github.personal@tbrowne.me>
2022-12-21 00:19:17 +0000

    Letter spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.body}>
           <div className={styles.largeText}>
-            <div className={styles.whiteText}>Hello, I&apos;m&nbsp;</div>
+            <div className={styles.whiteText}>Hello, I&apos;m</div>
             <div className={styles.greenText}>
               Tom
             </div>
